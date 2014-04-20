9b6b669a7247595d2249978e336ad003fe38646d
Steven Futter <futter.steven@gmail.com>
2014-04-19 22:34:18 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Refinerycmsapp::Application.config.secret_key_base = '7cc20a1a91ac679a97b83bcaf9ab92513d6b9a5494ecd887290001bf804f8e7433a1535c51169badebe6601e305a1e8638fe1d0e3331f361b3c7a1b43874d95d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Refinerycmsapp::Application.config.session_store :cookie_store, key: '_refinerycmsapp_session'
